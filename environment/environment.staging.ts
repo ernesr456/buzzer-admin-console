a594c4b05b5d3d6a0f3a140afe265949efb91b63
Ernesto Almario <50233898+ernesr456@users.noreply.github.com>
2026-07-28 20:31:24 +0800

    chore(config): add staging environment configuration with file replacements

---
+++ environment/environment.staging.ts
@@ -0,0 +1,6 @@
+// src/environments/environment.staging.ts
+export const environment = {
+  production: false,
+//   apiUrl: 'https://staging-api.yourdomain.com',
+  // ... other staging-specific variables
+};
