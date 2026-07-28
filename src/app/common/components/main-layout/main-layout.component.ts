81d285df9fe9dd165df089102769ed53c6b9b8bc
Ernesto Almario <50233898+ernesr456@users.noreply.github.com>
2026-07-29 01:02:36 +0800

    feat(layout): add router-outlet to render nested child routes

--- src/app/common/components/main-layout/main-layout.component.ts
+++ src/app/common/components/main-layout/main-layout.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../header/header.component';
 import { SidebarComponent } from '../../sidebar/sidebar.component';
 import { SidebarStateService } from '../../services/sidebar-state.service';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-main-layout',
   standalone: true,
-  imports: [CommonModule, HeaderComponent, SidebarComponent],
+  imports: [CommonModule, HeaderComponent, SidebarComponent, RouterOutlet],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
