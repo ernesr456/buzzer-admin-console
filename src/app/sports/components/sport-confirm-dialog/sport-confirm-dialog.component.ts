64f1302366271c46aea6627ca2a69f3c15673969
Ernesto Almario <50233898+ernesr456@users.noreply.github.com>
2026-07-29 21:40:58 +0800

    feat(sports): add sport confirm dialog component

---
+++ src/app/sports/components/sport-confirm-dialog/sport-confirm-dialog.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sport-confirm-dialog',
+  imports: [],
+  templateUrl: './sport-confirm-dialog.component.html',
+  styleUrl: './sport-confirm-dialog.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class SportConfirmDialogComponent {
+
+}
