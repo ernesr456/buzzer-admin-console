73f17031f45b90daece8529de1a38f4b12a3b98e
Ernesto Almario <50233898+ernesr456@users.noreply.github.com>
2026-07-29 17:50:00 +0800

    feat(app): protect main layout routes with auth guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { MainLayoutComponent } from './common/components/main-layout/main-layout
 import { MatchListComponent } from './matches/match-list/match-list.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
+import { authGuard } from './core/guards/auth/auth.guard';
 
 export const routes: Routes = [
   {
     path: '',
     component: MainLayoutComponent,
+    canActivate: [authGuard],
     title: 'Sport Management Dashboard',
     children: [
       {
