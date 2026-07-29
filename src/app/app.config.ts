d5d3648251b3dfd6c22dd2e65e89b20c82960637
Ernesto Almario <50233898+ernesr456@users.noreply.github.com>
2026-07-29 17:21:42 +0800

    feat(core): add http client with auth interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,15 @@
+// src/app/app.config.ts
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { authInterceptor } from './core/interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes), provideClientHydration(withEventReplay())
-  ]
+    provideRouter(routes),
+    provideHttpClient(
+      withInterceptors([authInterceptor])  // ✅ register the interceptor
+    ),
+  ],
 };
