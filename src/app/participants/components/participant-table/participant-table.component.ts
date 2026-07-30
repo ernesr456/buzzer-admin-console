9c86fb03ea4fc6f47f752129d03048fcb818c572
Ernesto Almario <50233898+ernesr456@users.noreply.github.com>
2026-07-31 02:55:49 +0800

    feat(participant): add participant table component

---
+++ src/app/participants/components/participant-table/participant-table.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-participant-table',
+  imports: [],
+  templateUrl: './participant-table.component.html',
+  styleUrl: './participant-table.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ParticipantTableComponent {
+
+}
