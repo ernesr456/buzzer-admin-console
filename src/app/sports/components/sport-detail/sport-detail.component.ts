e843886ac12c3da136f1c1717738164a662ca519
Ernesto Almario <50233898+ernesr456@users.noreply.github.com>
2026-07-31 01:56:35 +0800

    refactor(sport): remove change detectorref

--- src/app/sports/components/sport-detail/sport-detail.component.ts
+++ src/app/sports/components/sport-detail/sport-detail.component.ts
@@ -7,7 +7,6 @@ import { SportModel } from '../../models/sport.model';
 import { SportAddDialogComponent } from '../sport-add-dialog/sport-add-dialog.component';
 import { MatDialog } from '@angular/material/dialog';
 import { ToastService } from './../../../common/services/toast/toast.service';
-import { ChangeDetectorRef } from '@angular/core';
 import { EntityTableComponent } from '../../../entities/components/entity-table/entity-table.component';
 import { EntityModel } from '../../../entities/model/entity.model';
 import { CustomDialogComponent, CustomDialogData } from '../../../common/components/custom-dialog/custom-dialog.component';
