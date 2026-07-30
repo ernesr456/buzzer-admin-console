2d30913c05849e1faeda4f74e12e25aec6dbb80b
Ernesto Almario <50233898+ernesr456@users.noreply.github.com>
2026-07-30 15:48:51 +0800

    feat(sports): create organization detail component

---
+++ src/app/sports/components/organization-detail/organization-detail.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-organization-detail',
+  imports: [],
+  templateUrl: './organization-detail.component.html',
+  styleUrl: './organization-detail.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class OrganizationDetailComponent {
+
+}
