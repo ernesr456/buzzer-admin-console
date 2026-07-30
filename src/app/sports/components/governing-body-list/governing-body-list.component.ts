b3ca49ec39f80400c1cfd4c4af73ca710ea69c8b
Ernesto Almario <50233898+ernesr456@users.noreply.github.com>
2026-07-30 16:22:41 +0800

    feat(sports): add governing body list component

---
+++ src/app/sports/components/governing-body-list/governing-body-list.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-governing-body-list',
+  imports: [],
+  templateUrl: './governing-body-list.component.html',
+  styleUrl: './governing-body-list.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class GoverningBodyListComponent {
+
+}
