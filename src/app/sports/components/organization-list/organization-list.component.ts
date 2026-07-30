6c4c34d10a1ee9c1e02b59cf2f575083ae44b488
Ernesto Almario <50233898+ernesr456@users.noreply.github.com>
2026-07-30 16:22:59 +0800

    feat(sports) add organization list component

---
+++ src/app/sports/components/organization-list/organization-list.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-organization-list',
+  imports: [],
+  templateUrl: './organization-list.component.html',
+  styleUrl: './organization-list.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class OrganizationListComponent {
+
+}
