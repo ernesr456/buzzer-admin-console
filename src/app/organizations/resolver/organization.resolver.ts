ddb819bed3ebe0e0a10cb0841cd78ffc235e8db8
Ernesto Almario <50233898+ernesr456@users.noreply.github.com>
2026-07-31 19:46:25 +0800

    refactor(organization): implement shorter imports of service and model

--- src/app/organizations/resolver/organization.resolver.ts
+++ src/app/organizations/resolver/organization.resolver.ts
@@ -1,23 +1,18 @@
 // src/app/organizations/resolvers/organization.resolver.ts
-
 import { inject } from '@angular/core';
 import { ResolveFn, ActivatedRouteSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
-import { OrganizationModel } from '../../organizations/model/organization.model';
-import { OrganizationService } from '../../organizations/services/organization.service';
+import { OrganizationModel } from '../model/organization.model';
+import { OrganizationService } from '../services/organization.service';
 
 export const organizationResolver: ResolveFn<OrganizationModel | undefined> = (
   route: ActivatedRouteSnapshot
 ): Observable<OrganizationModel | undefined> => {
   const organizationService = inject(OrganizationService);
-
   const entityId = route.paramMap.get('entityId');
   const orgId = route.paramMap.get('orgId');
-
   if (!entityId || !orgId) {
-    // Return observable with undefined
     return new Observable(subscriber => subscriber.next(undefined));
   }
-
   return organizationService.getOrganizationById(entityId, orgId);
 };
