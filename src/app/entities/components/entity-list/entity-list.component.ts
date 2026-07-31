97743e6b1fc9f5075500ad0ed971ddb3928a047a
Ernesto Almario <50233898+ernesr456@users.noreply.github.com>
2026-07-31 15:28:24 +0800

    feat(entity): implement standalone in components

--- src/app/entities/components/entity-list/entity-list.component.ts
+++ src/app/entities/components/entity-list/entity-list.component.ts
@@ -2,11 +2,12 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-governing-body-list',
+  standalone: true,
   imports: [],
   templateUrl: './entity-list.component.html',
   styleUrl: './entity-list.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class EntityListComponent {
-
+  
 }
