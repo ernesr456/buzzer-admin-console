517955d1f2dfa028160b051b916bab8a41e439e5
Ernesto Almario <50233898+ernesr456@users.noreply.github.com>
2026-08-01 01:27:51 +0800

    refactor(app): remove the initialize services

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,12 +3,6 @@ import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ThemeService } from './common/services/theme/theme.service';
 import { ToastContainerComponent } from './common/components/custom-toast/toast-container.component';
-import { OrganizationService } from './organizations/services/organization.service';
-import { DataService } from './core/services/data/data.service'; // or SportDataService
-import { EntityService } from './entities/services/entity.service';
-import { ParticipantService } from './participants/services.service';
-import { SportsService } from './sports/services/sports/sports.service';
-
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, ToastContainerComponent],
@@ -20,19 +14,9 @@ export class App implements OnInit {
 
   constructor(
     private themeService: ThemeService,
-    private orgService: OrganizationService,
-    private dataService: DataService,
-    private entityService: EntityService,
-    private participantService: ParticipantService,
-    private sportsService: SportsService
   ) {}
 
   ngOnInit(): void {
-    const sports = this.dataService.loadSports();
-    this.orgService.initialize(sports);
-    this.entityService.initialize(sports);
-    this.participantService.initialize(sports);
-    this.sportsService.initialize(sports);
   }
 
   toggleTheme(): void {
