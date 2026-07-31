aacc2cd6f668ccc36f6145ff3bc228f5fc3f1eac
Ernesto Almario <50233898+ernesr456@users.noreply.github.com>
2026-08-01 03:07:25 +0800

    refactor(entity): optional organizations field

--- src/app/entities/model/entity.model.ts
+++ src/app/entities/model/entity.model.ts
@@ -2,10 +2,12 @@ import { OrganizationModel } from '../../organizations/model/organization.model'
 
 export interface EntityModel {
   id: string;
+  sportId?: string;
   name: string;
+  country?:string;
   logo?: string;
   createdAt: Date;
   updatedAt?: Date;
   onboardedAt?: Date;
-  organizations: OrganizationModel[];
+  organizations?: OrganizationModel[];
 }
