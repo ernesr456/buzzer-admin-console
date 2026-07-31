da9c8dfac67032b0c6ecca3ec56e1b247a64b3ee
Ernesto Almario <50233898+ernesr456@users.noreply.github.com>
2026-07-31 18:04:38 +0800

    refactor(entitiy) change date string to date in model

--- src/app/entities/model/entity.model.ts
+++ src/app/entities/model/entity.model.ts
@@ -1,11 +1,11 @@
-import { OrganizationModel } from "../../organizations/model/organization.model";
+import { OrganizationModel } from '../../organizations/model/organization.model';
 
 export interface EntityModel {
   id: string;
   name: string;
   logo?: string;
-  createdAt: string;
-  updatedAt?: string;
-  onboardedAt?: string;
+  createdAt: Date;
+  updatedAt?: Date;
+  onboardedAt?: Date;
   organizations: OrganizationModel[];
 }
