f828bbf9b426dbf4aee00781157e7b70b9014ac6
Ernesto Almario <50233898+ernesr456@users.noreply.github.com>
2026-08-02 00:27:53 +0800

    refactor(organization): remove logo field in model

--- src/app/organizations/model/organization.model.ts
+++ src/app/organizations/model/organization.model.ts
@@ -3,11 +3,10 @@ import { ParticipantModel } from "../../participants/model/participant.model";
 export interface OrganizationModel {
   id: string;
   name: string;
-  type?: string;
-  crestUrl?:string;
-  country?: string;
-  governingBodyId?:string;
-  logo?: string;
+  type: string;
+  crestUrl:string;
+  country: string;
+  governingBodyId:string;
   participants: ParticipantModel[];
   createdAt: Date;
   updatedAt: Date;
