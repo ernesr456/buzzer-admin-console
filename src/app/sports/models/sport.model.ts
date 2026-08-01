fad190459e43153be5949279142883594a3555a0
Ernesto Almario <50233898+ernesr456@users.noreply.github.com>
2026-08-01 22:41:30 +0800

    refactor(sport): remove emoji and color in model

--- src/app/sports/models/sport.model.ts
+++ src/app/sports/models/sport.model.ts
@@ -3,8 +3,6 @@ import { EntityModel } from "../../entities/model/entity.model";
 export interface SportModel {
   id: string;
   name: string;
-  emoji?: string;
-  color?: string;
   createdAt: Date;
   updatedAt?: Date;
   entities: EntityModel[];
