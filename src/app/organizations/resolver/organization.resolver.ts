8f7fd180ee1bd2d029a6e4f62ef33c81e3fe856b
Ernesto Almario <50233898+ernesr456@users.noreply.github.com>
2026-08-01 23:09:26 +0800

    refactor(organization): update the resolver

--- src/app/organizations/resolver/organization.resolver.ts
+++ src/app/organizations/resolver/organization.resolver.ts
@@ -1,18 +1,35 @@
-// src/app/organizations/resolvers/organization.resolver.ts
+// src/app/entities/resolver/entity.resolver.ts
 import { inject } from '@angular/core';
-import { ResolveFn, ActivatedRouteSnapshot } from '@angular/router';
-import { Observable } from 'rxjs';
+import { ResolveFn, ActivatedRouteSnapshot, Router } from '@angular/router';
+import { Observable, of } from 'rxjs';
+import { map, catchError, take } from 'rxjs/operators';
 import { OrganizationModel } from '../model/organization.model';
-import { OrganizationService } from '../services/organization.service';
+import {OrganizationService} from '../services/organization.service'
 
-export const organizationResolver: ResolveFn<OrganizationModel | undefined> = (
+export const entityResolver: ResolveFn<OrganizationModel | undefined> = (
   route: ActivatedRouteSnapshot
 ): Observable<OrganizationModel | undefined> => {
   const organizationService = inject(OrganizationService);
+  const router = inject(Router);
   const entityId = route.paramMap.get('entityId');
   const orgId = route.paramMap.get('orgId');
+
   if (!entityId || !orgId) {
-    return new Observable(subscriber => subscriber.next(undefined));
+    router.navigate(['/sports']);
+    return of(undefined);
   }
-  return organizationService.getOrganizationById(entityId, orgId);
+  return organizationService.getOrganizationsById(entityId,orgId).pipe(
+    take(1),
+    map(entity => {
+      if (!entity) {
+        router.navigate(['/sports']);
+        return undefined;
+      }
+      return entity;
+    }),
+    catchError(() => {
+      router.navigate(['/sports']);
+      return of(undefined);
+    })
+  );
 };
