8194289f01046bbeb7137709c34d073232053056
Ernesto Almario <50233898+ernesr456@users.noreply.github.com>
2026-08-03 15:18:28 +0800

    refactor(routes): create another routes for participants,squad, organization and staffs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,6 +10,11 @@ import { sportResolver } from './sports/resolver/sport.resolver';
 import { entityResolver } from './entities/resolver/entity.resolver';
 import { OrganizationDetailComponent } from './organizations/organization-detail/organization-detail.component';
 import { organizationResolver } from './organizations/resolver/organization.resolver';
+import { EntityListComponent } from './entities/entity-list/entity-list.component'
+import { OrganizationListComponent } from './organizations/organization-list/organization-list.component';
+import { ParticipantListComponent } from './participants/participant-list/participant-list.component';
+import { SquadListComponent } from './squad/squad-list/squad-list.component';
+import { StaffListComponent } from './staff/staff-list/staff-list.component';
 
 export const routes: Routes = [
   {
@@ -45,8 +50,63 @@ export const routes: Routes = [
       },
       {
         path: 'sports/:sportId/:entityId/:orgId',
+        title: 'Organization Management',
         component: OrganizationDetailComponent,
         resolve: { organization: organizationResolver },
+      },
+      {
+        path: 'entities',
+        component: EntityListComponent,
+        title: 'Sport Management',
+        canActivate: [authGuard],
+        data: { breadcrumb: 'Entities' },
+      },
+      {
+        path: 'entities/:sportId/:entityId',
+        component: EntityDetailComponent,
+        title: 'Entity Management',
+        canActivate: [authGuard],
+        resolve: { entity: entityResolver },
+      },
+      {
+        path: 'entities/:sportId/:entityId/:orgId',
+        title: 'Organization Management',
+        component: OrganizationDetailComponent,
+        resolve: { organization: organizationResolver },
+      },
+      {
+        path: 'organizations',
+        component: OrganizationListComponent,
+        title: 'Sport Management',
+        canActivate: [authGuard],
+        data: { breadcrumb: 'Organizations' },
+      },
+      {
+        path: 'organizations/:sportId/:entityId/:orgId',
+        title: 'Organization Management',
+        component: OrganizationDetailComponent,
+        resolve: { organization: organizationResolver },
+      },
+      {
+        path: 'participants',
+        component: ParticipantListComponent,
+        title: 'Sport Management',
+        canActivate: [authGuard],
+        data: { breadcrumb: 'Participants' },
+      },
+      {
+        path: 'squads',
+        component: SquadListComponent,
+        title: 'Sport Management',
+        canActivate: [authGuard],
+        data: { breadcrumb: 'Squads' },
+      },
+      {
+        path: 'staffs',
+        component: StaffListComponent,
+        title: 'Sport Management',
+        canActivate: [authGuard],
+        data: { breadcrumb: 'Staffs' },
       },
     ],
   },
