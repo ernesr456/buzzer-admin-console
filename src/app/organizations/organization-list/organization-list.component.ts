0eb80a888df05039ce53fbbbce798bb87a9f7837
Ernesto Almario <50233898+ernesr456@users.noreply.github.com>
2026-08-03 16:54:48 +0800

    feat(entity): implement breadcrumbs in organization list

--- src/app/organizations/organization-list/organization-list.component.ts
+++ src/app/organizations/organization-list/organization-list.component.ts
@@ -1,8 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CustomBreadcrumbsComponent } from '../../common/components/custom-breadcrumbs/custom-breadcrumbs.component';
 
 @Component({
   selector: 'app-organization-list',
-  imports: [],
+  imports: [CustomBreadcrumbsComponent],
   templateUrl: './organization-list.component.html',
   styleUrl: './organization-list.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
