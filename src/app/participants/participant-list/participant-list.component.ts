835c8284c074d2983c5ea2577d98b6545961c03b
Ernesto Almario <50233898+ernesr456@users.noreply.github.com>
2026-08-03 16:55:29 +0800

    feat(participant): implement breadcrumbs in participant list

--- src/app/participants/participant-list/participant-list.component.ts
+++ src/app/participants/participant-list/participant-list.component.ts
@@ -1,8 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CustomBreadcrumbsComponent } from '../../common/components/custom-breadcrumbs/custom-breadcrumbs.component';
 
 @Component({
   selector: 'app-participant-list',
-  imports: [],
+  imports: [CustomBreadcrumbsComponent],
   templateUrl: './participant-list.component.html',
   styleUrl: './participant-list.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
