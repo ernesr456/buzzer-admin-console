1c9d5db8dc77d01e316ae6051af4217e6d7c17f2
Ernesto Almario <50233898+ernesr456@users.noreply.github.com>
2026-08-03 16:54:34 +0800

    feat(entity): implement breadcrumbs in entity list

--- src/app/entities/entity-list/entity-list.component.ts
+++ src/app/entities/entity-list/entity-list.component.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CustomBreadcrumbsComponent } from '../../common/components/custom-breadcrumbs/custom-breadcrumbs.component';
 
 @Component({
   selector: 'app-governing-body-list',
   standalone: true,
-  imports: [],
+  imports: [CustomBreadcrumbsComponent],
   templateUrl: './entity-list.component.html',
   styleUrl: './entity-list.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
